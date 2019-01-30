c3a4c919012d419ab353dd6ce95f038cbe4be1c8
João Medeiros <jaum97@Yahoo.com.br>
2019-01-30 11:34:24 -0200

    Building the repo layout

---
+++ github-repo-display/src/components/RepoList/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const RepoList = styled.ul`
+  background-color: gray;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default RepoList;
